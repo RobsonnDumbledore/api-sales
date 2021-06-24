1c0b64eb1d7d2108ca5eac59ef08b76f07044f07
robsonndumbledore <robsonndumbledore@gmail.com>
2021-06-23 22:22:25 -0300

    feat: create a server

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,1 +1,11 @@
-console.log('Hello Dev!')
+import cors from 'cors'
+import express from 'express'
+import routes from '../routes'
+
+const app = express()
+
+app.use(cors())
+app.use(routes)
+app.use(express.json())
+
+app.listen(3333, () => console.log('server started on port 3333'))
