16dc23b1d2d8d5b417578ce2834287011f5ec8dc
robsonndumbledore <robsonndumbledore@gmail.com>
2021-06-23 22:56:45 -0300

    feat: create application error handling

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,11 +1,13 @@
 import cors from 'cors'
 import express from 'express'
 import routes from '../routes'
+import { handleError } from '../middlewares/HandleError'
 
 const app = express()
 
 app.use(cors())
 app.use(routes)
+app.use(handleError)
 app.use(express.json())
 
 app.listen(3333, () => console.log('server started on port 3333'))
---
+++ src/shared/middlewares/HandleError.ts
@@ -0,0 +1,21 @@
+import AppError from '../errors/AppError';
+import { NextFunction, Request, Response } from 'express';
+
+export function handleError(
+  error: Error,
+  request: Request,
+  response: Response,
+  next: NextFunction,
+) {
+  if (error instanceof AppError) {
+    return response.status(error.statusCode).json({
+      status: 'error',
+      message: error.message,
+    });
+  }
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+}
