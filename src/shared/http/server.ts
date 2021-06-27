41fc7f49ca9a48367f474557c310c9954c2393df
robsonndumbledore <robsonndumbledore@gmail.com>
2021-06-26 21:41:19 -0300

    feat: create product validation

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,5 +1,6 @@
 import '../typeorm';
 import cors from 'cors';
+import { errors } from 'celebrate';
 import express from 'express';
 import 'express-async-errors';
 import routes from '../routes';
@@ -10,7 +11,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
-
+app.use(errors())
 app.use(handleError);
 
 app.listen(3333, () => console.log('server started on port 3333 🏆'));
