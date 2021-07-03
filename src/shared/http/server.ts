2a5891343484ca185bbe7471b5d65d7e14e3883a
robsonndumbledore <robsonndumbledore@gmail.com>
2021-07-03 11:24:04 -0300

    feat: static route configuration for uploads

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -5,13 +5,15 @@ import express from 'express';
 import 'express-async-errors';
 import routes from '../routes';
 import { handleError } from '@shared/http/middlewares/HandleError';
+import uploadConfig from '../../config/upload';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.get('/files', express.static(uploadConfig.directory));
 app.use(routes);
-app.use(errors())
+app.use(errors());
 app.use(handleError);
 
 app.listen(3333, () => console.log('server started on port 3333 🏆'));
